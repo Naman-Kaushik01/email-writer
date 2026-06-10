17614ffe16f5616201cb72bf1b849650319f037b
Naman Kaushik <kaushiknaman68@gmail.com>
2026-06-11 02:32:49 +0530

    created BuildPrompt method

--- src/main/java/com/email/writer/app/EmailGeneratorService.java
+++ src/main/java/com/email/writer/app/EmailGeneratorService.java
@@ -4,5 +4,17 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class EmailGeneratorService {
+    public String generateEmailReply(EmailRequest emailRequest) {
+        //Build the prompt for the AI model
+        String prompt = buildPrompt(emailRequest);
+        //craft a request
+        //Do request and get response
+        // Return response
+    }
 
+    private String buildPrompt(EmailRequest emailRequest) {
+        StringBuilder prompt = new StringBuilder();
+        prompt.append("Generate a professional email reply for the following email content. Please don't generate the subject line ");
+        return null;
+    }
 }
