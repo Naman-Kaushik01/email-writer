021caaae9ecb484d028a5a35c681eaac9d04b612
user.name="Naman Kaushik <user.email = "kaushiknaman68@gmail.com>
2026-06-12 22:59:33 +0530

    working on frontend

--- Frontend/email-writer-react/src/App.jsx
+++ Frontend/email-writer-react/src/App.jsx
@@ -1,13 +1,25 @@
-
+import { useState } from 'react'
 import './App.css'
 
 function App() {
-  
+  const [emailContent, setEmailContent] = useState('');
+  const [tone, setTone] = useState('');
+  const [generatedReply, setGeneratedReply] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+
 
   return (
-    <>
-     <p>Hello</p>
-    </>
+  <Container maxWidth="md" sx={{py:4}}>
+      <Typography variant='h3' component="h1" gutterBottom>
+        Email Reply Generator
+      </Typography>
+
+     
+  </Container>
+
+
   )
 }
 
