17856eba5fef347587d550cdd4eb04313696e5cf
user.name="Naman Kaushik <user.email = "kaushiknaman68@gmail.com>
2026-06-12 23:28:51 +0530

    connected frontend with backend

--- Frontend/email-writer-react/src/App.jsx
+++ Frontend/email-writer-react/src/App.jsx
@@ -12,7 +12,20 @@ function App() {
 
 
   const handleSubmit = async () => {
-
+    setLoading(true);
+    setError('');
+    try {
+      const response = await axios.post("http://localhost:8080/api/email/generate", {
+       emailContent,
+       tone 
+      });
+      setGeneratedReply(typeof response.data === 'string' ? response.data : JSON.stringify(response.data));
+    } catch (error) {
+      setError('Failed to generate eamil reply. Please try again');
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
